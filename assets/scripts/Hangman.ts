f7176c15eec3befe59413153e3280ce0c60e541e
alexcstd <alex.scustod@gmail.com>
2022-01-16 16:54:54 -0300

    set up everything

--- assets/scripts/Hangman.ts
+++ assets/scripts/Hangman.ts
@@ -13,9 +13,21 @@ export default class Hangman extends cc.Component {
     button: cc.Node;
     word: cc.Node[];
     editBox: cc.Node;
+    // hidden word: rebirth
+    h_w: [string, string, string, string, string, string, string];
+    @property(cc.Label)
+    errorLabel: cc.Label = null;
+    @property(cc.Node)
+    anchor: cc.Node = null;
+    @property(cc.Node)
+    surprise: cc.Node = null;
+    errorPoints: number;
+    win: number;
+    hanged: cc.Node[];
     onLoad () {
-        // hidden word: rebirth
-        const h_w = ["R", "E", "B","I", "R", "T", "H"];
+        this.win = 0;
+        this.errorPoints = 0;
+        this.h_w = ["R", "E", "B","I", "R", "T", "H"];
         // conecting the editbox node
         this.editBox = this.node.getChildByName("editbox");
         //creating the button node and the first config to the word letters list
@@ -24,28 +36,66 @@ export default class Hangman extends cc.Component {
         this.word = [];
         //building the word letters list
         for (let i = 0;i<7;i++){
-            this.word.push([]);
+            this.word.push();
         }
         //adding nodes to the list
         labelNodes.forEach(b => {
             const x = parseInt(b.name);
             this.word[x] = b;
         });
+        this.buildHangedNodesList();
+        
     }
-    getStringFromLabel(w: cc.Node): string {
-        return w.getComponent(cc.Label).string;
+    buildHangedNodesList(){
+        const nodes = this.node.getChildByName("hanged").children;
+        this.hanged = [];
+        for (let i = 0; i<6;i++){
+            this.hanged.push();
+        }
+        nodes.forEach(b => {
+            const x = parseInt(b.name);
+            this.hanged[x] = b;
+        });
     }
-
     clickButton(){
-       const value = this.editBox.getChildByName("TEXT_LABEL").getComponent(cc.Label);
+       var aux = 0;
+       
+       const value = this.editBox.getChildByName("TEXT_LABEL").getComponent(cc.Label).string;
+       for (let i = 0; i < 7; i++){
+           if (this.h_w[i] == value.toUpperCase()){
+                
+                this.word[i].getComponent(cc.Label).string = value.toUpperCase();
+                aux += 1;
+                this.win += 1;
+                if (this.win == 7){
+                    this.endGameWinning();
+                }
+           }
+           else if (i == 6 && aux == 0){
+              this.errorPoints += 1;
+              this.errorLabel.string = `Erros: ${this.errorPoints}`
+              this.hanged[this.errorPoints - 1].active = true;
+              if (this.errorPoints == 6){
+                  this.endGameLosing();
+              }
+           }
+       }
        
     }
-    checkIfLetterExists(l: cc.Label){
-        for (let i = 0; i<7; i++){
-            if (this.h_w[i] = l){
-                
-            }
-        }
+    endGameLosing(){
+        this.editBox.active = false;
+        this.button.active = false;
+        const nodeLabel = new cc.Node("LabelTop");
+        nodeLabel.addComponent(cc.Label);
+        nodeLabel.getComponent(cc.Label).string = "You lose haahshuashusjsdkskdksaka";
+        this.anchor.addChild(nodeLabel);
+        setTimeout(() => {
+           this.anchor.removeAllChildren();
+        }, 2000);
+    }
+
+    endGameWinning(){
+        this.surprise.active = true;
     }
     start () {
 
